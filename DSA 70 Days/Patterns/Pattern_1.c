0d0c0f86155294b155920a25c4bf950e1b25176f
Mohan-I <mohanshyadav@gmail.com>
2026-01-23 23:18:26 +0530

    progress-2026-day16

---
+++ DSA 70 Days/Patterns/Pattern_1.c
@@ -0,0 +1,18 @@
+// AIM : TO PRINT PATTERN BELOW
+// *
+// **
+// ***
+// ****
+// *****
+
+#include <stdio.h>
+
+int main(){
+  for(int i = 1; i < 6; i++){
+      for(int j = 0; j < i; j++){
+          printf("*");
+      }printf("\n");
+  }
+  
+  return 0;
+ }
