ba8f9b87bb11bec1653050871c0c0ceb4e4afdfc
Mohan-I <mohanshyadav@gmail.com>
2026-02-17 21:09:11 +0530

    progress-day-33

--- Basic Algorithms/#Daily Sums/a.c
+++ Basic Algorithms/#Daily Sums/a.c
@@ -1,13 +1,29 @@
-#include <stdio.h>
+    // int nums[] = {0, 1, 2, 2, 3, 0, 4, 2};  v - 2
 
-int main(){
-    int i = 4, j = -1, k = 0, w,x,y,z;
+    #include <stdio.h>
 
-    w = i || j || k;
-    x = i && j && k;
-    y = i || j && k;
-    z = i && j || k;
+    int remEl(int* nums, int target, int sizeA){
+        int k = 0 ;
+        for(int i = 0; i < sizeA; i++){
+            
+        if( nums[i] != target){
+            nums[k] = nums[i];
+            k++;
+        }
+        }
 
-    printf("w = %d, x = %d, y = %d, z = %d",w,x,y,z);
-    return 0;
-}
+        return k;
+    };
+
+    int main(){
+        int nums[] = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        int size = sizeof(nums) / sizeof(nums[0]);
+
+        int numsSize = remEl(nums, val, size);
+
+        printf("New Size of Array: %d \n", numsSize);
+        for(int i = 0; i < numsSize; i++){
+            printf("[%d]", nums[i]);
+        }
+    }
