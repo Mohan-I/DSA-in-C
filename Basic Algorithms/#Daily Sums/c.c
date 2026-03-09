fb3477938d32091f70a04b4b34bf1d277f6f2552
Mohan-I <mohanshyadav@gmail.com>
2026-03-09 23:39:55 +0530

    progress-day-52

--- Basic Algorithms/#Daily Sums/c.c
+++ Basic Algorithms/#Daily Sums/c.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define TAB_SIZE 10
-
-typedef struct Node{
-    int key[50];
-    int value;
-    struct Node* next;
-} Node;
-
-Node* newNode[TAB_SIZE];
-unsigned int hash(char* key){
-    unsigned int value = 0;
-    for(int i = 0; i < strlen(key); i++){
-        value += value * i;
+void ascendNum(int n1, int n2){
+    if(n1 > n2) {
+        int temp = n1;
+        n1 = n2;
+        n2 = n1;
     }
-    return value % 12;
+}
+
+void divideNum(int nums[], int low, int high){
+    int low = 0;
+    int high = size - 1;
+        if(low < high){
+        int mid = low + (high - low) / 2;
+        divideNum(nums, low, mid);
+        divideNum(nums, mid + 1; high);
+    }
+}
+
+void writeNum(int arr[], int sz){
+    for(int i=0; i <sz; i++){
+        printf("[%d]",arr[i]);
+    }
 }
 
 int main(){
-    printf("MOHAN - > %d", hash("MOHAN"));
+    int nums[] = {22,23,42,53,36,73,84,69,39,49,59,96,97,49,29,34,45,64};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    writeNum(nums, size);
+    divideNum(nums, size);
+    writeNum(nums, size);
+
+
     return 0;
 }
