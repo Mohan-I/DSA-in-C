139d700b7c207a145c7b01ebc479c695d3fee56e
Mohan-I <mohanshyadav@gmail.com>
2026-03-10 23:24:46 +0530

    progress-day-53

---
+++ Basic Algorithms/unique concepts/quants/ref_math.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  printf("%f\n", exp(0));
+  printf("%f\n", exp(1));
+  printf("%f\n", exp(10));
+  printf("%f\n", exp(4.8));
+  printf("%f\n", exp(2.718));
+  return 0;
+}
