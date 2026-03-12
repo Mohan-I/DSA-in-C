188e7bcd4388c5587b50961abd61c10627518b6c
Mohan-I <mohanshyadav@gmail.com>
2026-03-12 23:53:18 +0530

    progress-day-54

--- Basic Algorithms/#Daily Sums/a.c
+++ Basic Algorithms/#Daily Sums/a.c
@@ -1,48 +1,38 @@
-/*
-A steel is graded according to the following conditions:
-
-(1) Hardness must be greater than 50
-(2) Carbon content must be less than 0.7
-(3) Tensile strength must be greater than 5600
-
-The grades are as follows:
-
-Grade is 10 if all three conditions are met
-Grade is 09 if conditions (1) and (2) are met
-Grade is 08 if conditions (2) and (3) are met
-Grade is 07 if conditions (1) and (3) are met
-Grade is 06 if only one condition is met
-Grade is 05 if none of the conditions are met 
-
-Write a program, which will require the user to give valuues of hardness, carbon content and tensile strength of the steel under consideration and output the grade of the steel.
-*/ 
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    float hardness, carbon, tensile;
-    printf("\nEnter the hardness level : ");
-    scanf("%f", &hardness);
-    printf("\nEnter the carbon rate : ");
-    scanf("%f", &carbon);
-    printf("\nEnter the tensile strength : ");
-    scanf("%f", &tensile);
-    
-    printf("\n--------------\n");
-
-    if(hardness > 50 && carbon < 0.7 && tensile > 5600){
-        printf("Grade 10");
-    }else if(hardness > 50 && carbon < 0.7){
-        printf("Grade 09");
-    }else if(carbon < 0.7 && tensile > 5600){
-        printf("Grade 08");
-    }else if(hardness > 50 && tensile > 5600){
-        printf("Grade 07");
-    }else if(hardness > 50 || carbon < 0.7 || tensile > 5600){
-        printf("Grade 06");
-    }else{
-        printf("Grade 05");
+int binarySearch(int arr[], int size, int target){
+    int low = 0;
+    int high = size - 1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[mid] < target ){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
     }
+
+    return -1;
+}
+
+void writeNums(int arr[], int sz){
+    for(int i = 0; i < sz; i++){
+        printf("[%d]",arr[i]);
+    }
+}
+
+int main(){
+    int nums[] = {11,21,31,41,51,61,71,81,91,101,111,112,113,114,115,116,117,118};
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int target = 31;
+
+    writeNums(nums, size);
+    int index = binarySearch(nums, size, target);
+
+    printf("\n The Element was found at index : %d", index);
     return 0;
 }
