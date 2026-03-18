6f4c9fafca07524badce8ac11cbea6434113f5e8
Mohan-I <mohanshyadav@gmail.com>
2026-03-18 22:19:31 +0530

    progress-day-56

--- Basic Algorithms/#Daily Sums/b.c
+++ Basic Algorithms/#Daily Sums/b.c
@@ -1,35 +1,62 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+}
+
+struct Node* head = NULL;
+
+void insertNode(int user_data){
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode -> data = user_data;
+    newnode -> next = head;
+    head = newNode;
+
+    printf("\n[the node has been created !]");
+}
+
+void showNodes(){
+    struct Node* temp = head;
+    while(temp != NULL){
+        printf("-=[%d]=-", temp->data);
+        temp = temp->next;
+    }
+}
+
+void removeNode(int key){
+    
+}
 
 int main(){
-    
-    float hardness, carbon, tensile;
-    printf("\nEnter the hardness level : ");
-    scanf("%f", &hardness);
-    printf("\nEnter the carbon rate : ");
-    scanf("%f", &carbon);
-    printf("\nEnter the tensile strength : ");
-    scanf("%f", &tensile);
-    
-    printf("\n--------------\n");
+    int choice, element;
+    while(1){
+        printf("\n[----Linked List----]");
+        printf("\n1.Insert\n2.remove\n3.Show\n4.Exit");
+        printf("\nEnter the choice: ");
+        scanf("%d", &choice);
 
-    bool hardnessTrue = (hardness > 50);
-    bool carbonRtTrue = (carbon< 0.7);
-    bool tensile_True = (tensile > 5600);
+        switch(choice){
+            case 1:
+            printf("\nEnter The node :");
+            scanf("%d", &element);
+            insertNode(element);
+            break;
 
-    if(hardnessTrue && carbonRtTrue && tensile_True){
-        printf("Grade 10");
-    }else if(hardnessTrue && carbonRtTrue){
-        printf("Grade 09");
-    }else if(carbonRtTrue && tensile_True){
-        printf("Grade 08");
-    }else if(carbonRtTrue && tensile_True){
-        printf("Grade 07");
-    }else if(hardnessTrue || carbonRtTrue || tensile_True){
-        printf("Grade 06");
-    }else{
-        printf("Grade 05");
+            case 2:
+            printf("\nRemoving Node........");
+            break;
+
+            case 3:
+            printf("\n[===ALL NODES===]\n");
+            showNodes();
+            break;
+
+            case 4:
+            return 0;
+            
+            default: printf("\nINCORRECT CHOICE");
+        }
     }
-
-    return 0;
 }
