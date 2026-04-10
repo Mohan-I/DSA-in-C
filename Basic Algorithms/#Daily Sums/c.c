643cf1e8b9bb214f787ecae8a3ff0bd599f42a4a
Mohan-I <mohanshyadav@gmail.com>
2026-04-10 20:34:57 +0530

    progress-day-65

--- Basic Algorithms/#Daily Sums/c.c
+++ Basic Algorithms/#Daily Sums/c.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void moveZeros(int nums[], int sz){
-    int lastNonZero = 0;
-
-    // Phase 1: Move all non-zero elements to the front
-    for (int i = 0; i < sz; i++) {
-        if (nums[i] != 0) {
-            // Swap the current non-zero element with the element at lastNonZero
-            int temp = nums[i];
-            nums[i] = nums[lastNonZero];
-            nums[lastNonZero] = temp;
-            
-            printf("%d -> %d \n", nums[i], nums[lastNonZero]);
-
-            lastNonZero++;
-
+void selectionSort(int Arr[], int size){
+    for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            if(Arr[j] < Arr[i]){
+                int temp = Arr[i];
+                Arr[i] = Arr[j];
+                Arr[j] = temp;
+            }
         }
     }
 }
 
-void printNums(int arr[], int sz){
-    
+void printNums(int Arr[], int size){
+    for(int i = 0; i < size; i++){
+        printf("[%d]", Arr[i]);
+    }
 }
 
 int main(){
-    int nums[] = {1,0,3,0,4,0,0,1,2};
+    int nums[] = {2,5,3,4,1,7,9,8};
     int size = sizeof(nums) / sizeof(nums[0]);
 
-    moveZeros(nums, size);
-
-    for(int i = 0; i < size; i++){
-        printf("[%d]", nums[i]);
-    }
+    selectionSort(nums, size);
+    printNums(nums, size);
 
     return 0;
 }
--- Basic Algorithms/#Daily Sums/e.c
+++ Basic Algorithms/#Daily Sums/e.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 
+
 int main() {
     char binary[25], ch;
 
