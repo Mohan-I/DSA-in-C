27eff1400527e134292679703b0e6a776f7d66a9
Mohan-I <mohanshyadav@gmail.com>
2026-05-26 23:26:47 +0530

    progress-day-83

--- Basic Algorithms/#Daily Sums/a.c
+++ Basic Algorithms/#Daily Sums/a.c
@@ -1,83 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#define SIZE 10
-
-int front = -1, rear = -1, ready_q[SIZE];
-
- bool isFull(){
-    return (rear + 1) % SIZE == front;
- }
-
- bool isEmpty(){
-    return front == -1;
- }
-
-void enqueue(){
-    if(isFull()){
-        printf("\nTHE QUEUE IS FULL");
-    }else{
-        int data;
-        if(front == -1) front = 0;
-        printf("Enter Value to Add in Queue : ");
-        scanf("%d", &data);
-        rear = (rear + 1) % SIZE;
-        ready_q[rear] = data;
-    }
-}
-
-void dequeue(){
-    if(isEmpty()){
-        printf("\nTHE QUEUE IS EMPTY");
-    }else{
-        if(rear == front){
-            rear = -1; 
-            front = -1;
-        }
-        printf("\n Removing Item: [%d]", ready_q[front]);
-        front = (front + 1) % SIZE;
-    }
-}
-
-void show(){
-    if(isEmpty()){
-        printf("\nTHE QUEUE IS EMPTY");
-    }else{
-        printf("\n== ITEMS IN QUEUE ==\n");
-        int i = front;
-        while(1){
-            printf("[%d]", ready_q[i]);
-            if(i == rear)  break;
-            i = (i + 1)  % SIZE;
-        }
-        printf("\nFRONT: %d , REAR : %d ", front, rear);
-    }
-}
+#include <string.h>
 
 int main(){
-    int choice;
-    while(1){
-        printf("\n[==== CIRCULAR QUEUE ====]");
-        printf("\n1.Enqueue\n2.Dequeue\n3.Show\n4.Exit");
-        printf("\nEnter Operation to perform :");
-        scanf("%d", &choice);
-        switch(choice){
-            case 1:
-            enqueue();
-            break;
+    char text[20], newtext[20];
+    printf("[w] :");
+    
+    scanf("%19s", text);
 
-            case 2:
-            dequeue();
-            break;
-
-            case 3:
-            show();
-            break;
-
-            case 4:
-            exit(0);
-
-            default : printf("\n[==INCORRECT CHOICE==]");
+    int j = 0;
+    for(int i = 0; text[i] != '\0' ; i++){
+        if(text[i] == 'a' ||text[i] == 'e' ||text[i] == 'i' ||text[i] == 'o' ||text[i] == 'u'){
+            printf("Removed Vowel : %c \n", text[i]);
+        }else{
+            printf("Constant stored: %c \n", text[i]);
+            newtext[j] = text[i];
+            j++;
         }
     }
+
+    newtext[j] = '\0';
+
+    printf("%s", newtext);
+    return 0;
 }
