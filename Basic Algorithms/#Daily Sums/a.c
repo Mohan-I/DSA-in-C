c135a447a8e5295c969b8dfc68b92cb33b0975ff
Mohan-I <mohanshyadav@gmail.com>
2026-07-20 23:53:54 +0530

    progress-day-95

--- Basic Algorithms/#Daily Sums/a.c
+++ Basic Algorithms/#Daily Sums/a.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    char l = 65;
+    char *s ="abcedeb";
+    int len = strlen(s);
+    
+    // 1. Track total occurrences of each character
+    int count[26] = {0};
+    for (int i = 0; i < len; i++) {
+        printf("%c -> %d\n", s[i], s[i]);
+        count[s[i] - 'a']++;
+    }
 
-    for(int i = 1; i < 27; i++){
-        printf("%c ", l++);
-        if(i % 4 == 0) printf("\n");
+    for(int i = 0; i < len; i++){
+        printf("%d ", count[s[i] - 'a']);
     }
 
     return 0;
